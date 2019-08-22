b3be24e83523e3eb8b11affeda9c4c3927d968c8
pappda <paddfedwda@gmail.com>
2019-08-22 10:21:03 +0200

    add currency balance widget

---
+++ lib/currency_balance.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class CurrencyBalance extends StatefulWidget {
+  double visibility;
+  CurrencyBalance({Key key, this.visibility = 1}) : super(key: key);
+
+  @override
+  _CurrencyBalanceState createState() {
+    return _CurrencyBalanceState(visibility);
+  }
+}
+
+class _CurrencyBalanceState extends State<CurrencyBalance> {
+  final double visibility;
+  int selectedValue = 1;
+
+  _CurrencyBalanceState(this.visibility);
+
+  @override
+  Widget build(BuildContext context) {
+    //print(visibility.toString());
+    return CurrencyPicker(
+      onChanged: (value) => setState(() => selectedValue = value),
+      listViewWidth: 350,
+      selectedValue: selectedValue,
+      visibilityFactor: visibility,
+    );
+  }
+}
+
+class CurrencyPicker extends StatelessWidget {
+  final List<double> balances = [125, 59875, 687, 741, 22];
+  final List<String> longCurrencyNames = [
+    "Euro",
+    "Hungarian Forint",
+    "American Dollar",
+    "Vmi mas",
+    "Megin mas"
+  ];
+  final List<String> shortCurrencyNames = ["€", "Ft", "\$", "Vm", 'Mm'];
+  final double maxFontSize;
+  final double minFontSize;
+
+  final double visibilityFactor;
+
+  ///called when selected value changes
+  final ValueChanged<num> onChanged;
+
+  ///Width of list view
+  final double listViewWidth;
+
+  ///Currently selected index of the value
+  final int selectedValue;
+
+  final ScrollController _scrollController;
+
+  CurrencyPicker(
+      /* this.balances, this.longCurrencyNames, this.shortCurrencyNames, */
+      {Key key,
+      this.maxFontSize = 48,
+      this.minFontSize = 18,
+      this.visibilityFactor = 1,
+      this.onChanged,
+      this.listViewWidth,
+      this.selectedValue})
+      : _scrollController = ScrollController(
+          initialScrollOffset: (selectedValue) * listViewWidth / 3,
+        ),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final fontSize =
+        maxFontSize - visibilityFactor * (maxFontSize - minFontSize);
+    final defaultStyle = TextStyle(
+        fontSize: fontSize, color: Colors.grey.withOpacity(visibilityFactor));
+    final selectedStyle = TextStyle(fontSize: fontSize, color: Colors.black);
+
+    int itemCount = balances.length + 2;
+    return NotificationListener(
+      child: Container(
+        width: listViewWidth,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          controller: _scrollController,
+          itemExtent: listViewWidth / 3,
+          itemCount: itemCount,
+          itemBuilder: (BuildContext context, int index) {
+            final value = index - 1;
+
+            //define special style for selected (middle) element
+            final itemStyle =
+                value == selectedValue ? selectedStyle : defaultStyle;
+
+            bool isExtra = index == 0 || index == itemCount - 1;
+
+            return isExtra
+                ? Container() //empty first and last element
+                : Center(
+                    child: Column(
+                      children: <Widget>[
+                        Text("${balances[value]} ${shortCurrencyNames[value]}",
+                            style: itemStyle),
+                        Text("${longCurrencyNames[value]}",
+                            style: defaultStyle.merge(TextStyle(fontSize: 14))),
+                      ],
+                    ),
+                  );
+          },
+        ),
+      ),
+      onNotification: _onNotification,
+    );
+  }
+
+  bool _onNotification(Notification notification) {
+    if (notification is ScrollNotification) {
+      //calculate
+      int intIndexOfMiddleElement =
+          (notification.metrics.pixels + listViewWidth / 2) ~/
+              (listViewWidth / 3);
+      int intValueInTheMiddle = intIndexOfMiddleElement - 1;
+      print(intIndexOfMiddleElement.toString());
+
+      if (_userStoppedScrolling(notification, _scrollController)) {
+        //center selected value
+        _scrollController.animateTo(intValueInTheMiddle * (listViewWidth / 3),
+            duration: Duration(milliseconds: 1000), curve: ElasticOutCurve());
+      }
+
+      //update selection
+      if (intValueInTheMiddle != selectedValue) {
+        num newValue;
+
+        //return integer value
+        newValue = (intValueInTheMiddle);
+
+        onChanged(newValue);
+      }
+    }
+    return true;
+  }
+
+  ///indicates if user has stopped scrolling so we can center value in the middle
+  bool _userStoppedScrolling(
+      Notification notification, ScrollController scrollController) {
+    return notification is UserScrollNotification &&
+        notification.direction == ScrollDirection.idle &&
+        scrollController.position.activity is! HoldScrollActivity;
+  }
+}
